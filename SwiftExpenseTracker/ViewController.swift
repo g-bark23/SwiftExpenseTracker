e593d3613137f1dc75f702e5b9cbe23d54bcc14c
pharrett23 <pharrett23@gmail.com>
2017-10-04 10:08:53 -0600

    Initial Commit

---
+++ SwiftExpenseTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftExpenseTracker
+//
+//  Created by Garrett Barker on 10/4/17.
+//  Copyright © 2017 Garrett Barker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
